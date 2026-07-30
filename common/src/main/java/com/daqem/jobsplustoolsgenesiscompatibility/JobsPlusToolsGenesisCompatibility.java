3fbd26bf196efd7695265f9306b5e0a08bd50b83
DAQEM <66999025+DAQEM@users.noreply.github.com>
2026-07-30 23:06:04 +0200

    feat: initial commit

---
+++ common/src/main/java/com/daqem/jobsplustoolsgenesiscompatibility/JobsPlusToolsGenesisCompatibility.java
@@ -0,0 +1,8 @@
+package com.daqem.jobsplustoolsgenesiscompatibility;
+
+public class JobsPlusToolsGenesisCompatibility {
+    public static final String MOD_ID = "jobsplustoolsgenesiscompatibility";
+
+    public static void init() {
+    }
+}
